3a5be57af7d0c2fedf8857a3d7f530eaff158fbf
Andre MARQUEZ <amarquez@e4r1p4.wethinkcode.co.za>
2017-11-07 11:22:24 +0200

    Added terminal

---
+++ 00_how_to_docker/08.sh
@@ -0,0 +1,5 @@
+# Launch a shell from an alpine container
+# and make sure that you can interact
+# directly with the container via your terminal
+
+docker run --interactive --tty --rm alpine /bin/sh

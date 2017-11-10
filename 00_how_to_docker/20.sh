a736f54bd02f6b669261776803c25c4f8069c45c
Andre MARQUEZ <amarquez@e4r1p4.wethinkcode.co.za>
2017-11-10 15:37:15 +0200

    add new docker-machine

---
+++ 00_how_to_docker/20.sh
@@ -0,0 +1,3 @@
+# create a local swarm with Char as manager
+
+docker swarm init --advertise-addr $(./02.sh):2377
---
+++ 00_how_to_docker/21.sh
@@ -0,0 +1,3 @@
+# Create another virtual machine with docker-machine using virtualbox driver
+
+docker-machine create --driver=virtualbox Aiur

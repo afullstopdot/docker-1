3352a49ced63819dd95c4b1005f361fd1a11c5b9
Andre MARQUEZ <amarquez@e4r1p4.wethinkcode.co.za>
2017-11-10 15:14:17 +0200

    19 works

---
+++ 00_how_to_docker/19.sh
@@ -0,0 +1,28 @@
+# launch a container named Abathur
+
+docker run --name Abathur -v $HOME/root:/root -p 3000:3000 --interactive --tty python:2.7.13-slim python
+
+# import os
+
+# import os
+
+# pip install flask
+
+# os.system('pip install Flask')
+
+# import
+
+# from flask import Flask
+
+# app var
+
+# app = Flask(__name__)
+
+# define
+
+# def hello():
+#    return "<h1>Hello World</h1>"
+
+# run app
+
+# app.run(host='0.0.0.0'. port=3000)
